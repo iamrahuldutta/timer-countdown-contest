3de066cb0679474f0f27e58d6736ffdfd168c9e7
Rahul D <iamrahuldutta@gmail.com>
2024-03-05 14:02:06 +0530

    Adding a dialog for result

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,39 +1,46 @@
 import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
   const timer = useRef();
+  const result = useRef();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      result.current = "Lost";
     }, targetTime * 1000);
     setTimerStarted(true);
   }
 
   function handleStop() {
+    result.current = "Won";
     setTimerStarted(false);
     clearTimeout(timer.current);
   }
 
   return (
-    <section className="challenge">
-      <p>{timerExpired ? "You Lost" : ""}</p>
-      <h2>
-        {title}
-        <p className="challenge-time">
-          {targetTime} second{targetTime > 1 ? "s" : ""}
-        </p>
-        <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
-          </button>
-        </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
-        </p>
-      </h2>
-    </section>
+    <>
+      {timerExpired && <ResultModal result="lost" targetTime={targetTime} />}
+
+      <section className="challenge">
+        <h2>
+          {title}
+          <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? "s" : ""}
+          </p>
+          <p>
+            <button onClick={timerStarted ? handleStop : handleStart}>
+              {timerStarted ? "Stop" : "Start"} Challenge
+            </button>
+          </p>
+          <p className={timerStarted ? "active" : undefined}>
+            {timerStarted ? "Time is running..." : "Timer inactive"}
+          </p>
+        </h2>
+      </section>
+    </>
   );
 }
